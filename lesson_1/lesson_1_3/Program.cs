03f1efdfcfea2798c4349afa6f6757482f285d0d
Камила Боровикова <kamale1792@mail.ru>
2022-12-06 20:14:51 +0300

    задания по 3 лекции

---
+++ lesson_1/lesson_1_3/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+internal class Program
+{
+private static void Main(string[] args)
+{
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= number; i++)
+
+Console.Write($"{Math.Pow(i, 3)}. ");
+}
+}
